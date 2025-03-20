2dbcfa0c4e47e41ef044bc03a5a2e7667cd077a2
Cesar-CAAR <cesar.andres5242@gmail.com>
2025-03-19 22:39:48 -0700

    Enlazar Vistas

---
+++ View/Login.xaml.cs
@@ -0,0 +1,13 @@
+using ProyectoBomba.ViewModel;
+
+namespace ProyectoBomba.View
+{
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+            BindingContext = new LoginViewModel();
+        }
+    }
+}
---
+++ ViewModel/CrearCuentaViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Input;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+using ProyectoBomba.View;
+
+namespace ProyectoBomba.ViewModel
+{
+    public class CrearCuentaViewModel
+    {
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public ICommand CrearCuentaCommand { get; }
+
+        public CrearCuentaViewModel()
+        {
+            CrearCuentaCommand = new Command(async () => await CrearCuenta());
+        }
+
+        private async Task CrearCuenta()
+        {
+            // Simulación de creación de cuenta
+            await Application.Current.MainPage.DisplayAlert("Éxito", "Cuenta creada correctamente", "OK");
+
+            await Application.Current.MainPage.Navigation.PushAsync(new Login());
+        }
+    }
+}
